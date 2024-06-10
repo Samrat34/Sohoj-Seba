cf8092b90dd9630877351ce8cf820c5194bc2e67
Mehedi Hasan Samrat <mehedihasansamrat.mhs01@gmail.com>
2024-06-10 09:54:10 +0600

    initial commiet

---
+++ assets/js/new.js
@@ -0,0 +1,8 @@
+function togglePopup() {
+    var popup = document.getElementById("popupMenu");
+    if (popup.style.display === "none" || popup.style.display === "") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
